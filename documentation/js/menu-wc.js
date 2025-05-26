084cdbb21a5c51209597852cec27f25c16e91163
Biagio Scaglia <scagliabiagio99@gmail.com>
2025-05-26 14:17:46 +0200

    secondo commit

---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,167 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">super-mario documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CardComponent.html" data-type="entity-link" >CardComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CookieBannerComponent.html" data-type="entity-link" >CookieBannerComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DemoComponent.html" data-type="entity-link" >DemoComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FooterComponent.html" data-type="entity-link" >FooterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FormDataComponent.html" data-type="entity-link" >FormDataComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GameComponent.html" data-type="entity-link" >GameComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GamesListComponent.html" data-type="entity-link" >GamesListComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GiochiComponent.html" data-type="entity-link" >GiochiComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GiocoDettaglioComponent.html" data-type="entity-link" >GiocoDettaglioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MusicComponent.html" data-type="entity-link" >MusicComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MyButtonComponent.html" data-type="entity-link" >MyButtonComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NavbarComponent.html" data-type="entity-link" >NavbarComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NotfoundComponent.html" data-type="entity-link" >NotfoundComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PersonaggiComponent.html" data-type="entity-link" >PersonaggiComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PersonaggioDettaglioComponent.html" data-type="entity-link" >PersonaggioDettaglioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PowerupdettaglioComponent.html" data-type="entity-link" >PowerupdettaglioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PowerupsComponent.html" data-type="entity-link" >PowerupsComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/TuboMarioComponent.html" data-type="entity-link" >TuboMarioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/UserDashboardComponent.html" data-type="entity-link" >UserDashboardComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/Game.html" data-type="entity-link" >Game</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/NavItem.html" data-type="entity-link" >NavItem</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/UserInfo.html" data-type="entity-link" >UserInfo</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
